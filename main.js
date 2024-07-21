591133288d80bab7356227d031f75996ac8239d9
zeeshanahmedpk <za219285@gmail.com>
2024-07-22 03:52:19 +0500

    variable pushed

---
+++ main.js
@@ -0,0 +1,6 @@
+var favoritecolor = "green";
+console.log(favoritecolor);
+var greeting = "hello world";
+console.log(greeting);
+var number = 2000;
+console.log(number);
---
+++ main.ts
@@ -0,0 +1,8 @@
+let favoritecolor = "green"
+console.log(favoritecolor)
+
+let greeting = "hello world"
+console.log(greeting)
+
+let number = 2000
+console.log(number)
